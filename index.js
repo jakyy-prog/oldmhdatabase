33c7fba12b9427cc86ad0932467827dc1366c9b7
vzaki <146656230+jakyy-prog@users.noreply.github.com>
2026-03-09 15:56:35 +0700

    Memperbaiki index.js

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ app.get("/quest/:game/:type/:stars", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("API running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`API running on port ${PORT}`);
 });
