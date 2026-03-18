e03eff4a732524a1daa9531fb56f38bf1e4f75e0
vzaki <146656230+jakyy-prog@users.noreply.github.com>
2026-03-18 16:36:46 +0700

    update: menambahkan data untuk mhfu, dan perubahan format tabel

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
-const fs = require("fs");
-const path = require("path");
 
 const app = express();
 app.use(cors());
@@ -35,7 +33,42 @@ app.get("/quest/:game/:type/:stars", (req, res) => {
   });
 });
 
+app.get("/quest/:game/:type/stars/:stars", (req, res) => {
+  const { game, type, stars } = req.params;
+
+  const db = databases[game];
+  if (!db) return res.status(404).json({ error: "Game tidak ditemukan" });
+  if (type !== "village" && type !== "guild")
+    return res.status(400).json({ error: "Type tidak valid" });
+
+  db.all(`SELECT * FROM ${type} WHERE stars = ?`, [stars], (err, rows) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(rows);
+  });
+});
+
+app.get("/quest/:game/:type/ranks/:rank", (req, res) => {
+  const { game, type, rank } = req.params;
+
+  const db = databases[game];
+  if (!db) return res.status(404).json({ error: "Game tidak ditemukan" });
+  if (type !== "village" && type !== "guild")
+    return res.status(400).json({ error: "Type tidak valid" });
+
+  db.all(
+    `SELECT * FROM ${type} WHERE ranks = ? AND (type = 'Key Quest' OR type = 'Urgent Quest')`,
+    [rank],
+    (err, rows) => {
+      if (err) return res.status(500).json({ error: err.message });
+      res.json(rows);
+    },
+  );
+});
+
 app.get("/files", (req, res) => {
+  const fs = require("fs");
+  const path = require("path");
+
   function listFiles(dir) {
     const result = {};
     const items = fs.readdirSync(dir);
